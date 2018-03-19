97522943e31323d9eb61721b697259857a7ee627
Chris Raveendra <craveend@umich.edu>
2018-03-19 14:35:21 -0700

    Adding Editor component and beginning of styling

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Toolbar from './Toolbar';
+import TextEditor from './TextEditor';
 
 export default class App extends React.Component {
   render() {
     return (<MuiThemeProvider><div>
       <Toolbar />
+
       <h2>Welcome to React!</h2>
+
+      <TextEditor />
     </div>
     </MuiThemeProvider>);
   }
