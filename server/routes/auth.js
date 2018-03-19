15cc6ffacdc830d7b03602da207b3721b60bbcc2
Persis Ratouis <persis.ratouis@gmail.com>
2018-03-19 13:09:58 -0700

    working on backend and authentication

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,45 @@
 import express from 'express';
 const router = express.Router();
+import { User } from '../models/models';
+import crypto from 'crypto';
+import bodyParser from 'body-parser';
 
-module.exports = function(passport) {
+const hashPassword = (secret) => {
+  let hash = crypto.createHash('sha256');
+  hash.update(secret);
+  return hash.digest('hex');
+}
 
+module.exports = {
+  secretRouter: function(passport) {
+    router.post('/signup', (req, res) => {
+      // TODO: this is assuming structure about front-end
+      // we are assuming backend will be passed four things
+      // 1. email
+      // 2. username
+      // 3. password
+      // 4. repeatPassword
+      if(req.body.password !== req.body.repeatPassword) {
+        return {
+          error: "Passwords don't match"
+        }
+      }
+
+      let newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashPassword(req.body.password)
+      });
+
+      newUser.save()
+      .then(user => user)
+      .catch((err) => {
+        console.log('Error in signup passport\n'+err);
+        return {
+          status: 500,
+          error: err
+        }
+      })
+    })
+  }
 }
