6ee878ce7a8f9b9f2c0ac8e4354c4a2a9f7b7274
Chris Raveendra <craveend@umich.edu>
2018-03-19 15:31:36 -0700

    Updates to TextEditor & overall page logic & styling

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { connect } from 'react-redux';
 import { Editor, EditorState, RichUtils } from 'draft-js';
-
+import Paper from 'material-ui/Paper';
 
 export default class TextEditor extends React.Component {
     constructor(props) {
@@ -21,12 +21,13 @@ export default class TextEditor extends React.Component {
             <div id='content'>
                 <h1>Draft.js Editor</h1>
                 <button onClick={this._onBoldClick.bind(this)}>Bold</button>
-                <div className='editor'>
+                <Paper  zDepth={5}>
                     <Editor
+                        className='editor'
                         editorState={this.state.editorState}
                         onChange={this.onChange}
                     />
-                </div>
+                </Paper>
             </div >
     );
     }
