9197cac11309458221e03c8fcfc7fa1745f67972
Persis Ratouis <persis.ratouis@gmail.com>
2018-03-19 17:08:51 -0700

    passport and sessions;

--- server/models/models.js
+++ server/models/models.js
@@ -1,58 +1,13 @@
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
 
 
-let userSchema = mongoose.Schema({
-  email: [String],
-  username: [String],
-  password: [String]
+const userSchema = mongoose.Schema({
+  email: String,
+  username: String,
+  password: String
 });
 
-
-/*
-*  compare passed in password with the value in the database
-*   @param {string} password from react frontend
-*   @returns {function} cb from TODO where?
-*/
-userSchema.methods.comparePassword = function comparePassword(password, cb) {
-  bcrypt.compare(password, this.password, cb);
-}
-
-
-/*
- * convert password to hash beforehand
- *  @ param {function} next from TODO where?
- */
-userSchema.pre('save', function saveHook(next) {
-  const user = this;
-  // TODO what does this actually do
-  // something about checking if password is modified or if new user
-  if (!user.isModified('password')) return next();
-
-  // this is similar to hashing password with crypto in passport local strategy
-  return bcrypt.genSalt((saltError, salt) => {
-    // report saltError
-    if (saltError) { return next(saltError); }
-
-    return bcrypt.hash(user.password, salt, (hashError, hash) => {
-      // report hashError
-      if (hashError) { return next(hashError); }
-      // update password to hashed version
-      user.password = hash;
-      // continue
-      return next();
-    });
-  });
-});
-
-/* TODO: */
-// let documentSchema = mongoose.Schema({
-//  htmltext: [String]
-// })
-
-
-const User = mongoose.model('User', userSchema);
 
 module.exports = {
-  User: User
+  User: mongoose.model('User', userSchema)
 };
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,65 @@
 import express from 'express';
+import crypto from 'crypto';
+import { User } from '../models/models';
+
+
+function hashPassword(password) {
+  const hash = crypto.createHash('md5');
+  hash.update(password);
+  return hash.digest('hex');
+}
 const router = express.Router();
 
-module.exports = function(passport) {
+// TODO: include validation
+module.exports = {
+  authRouter: function (passport) {
+    router.post('/signup', (req, res, next) => {
+      // TODO confirm that validation happens on frontend
+      const newUser = new User({
+        email: req.body.email,
+        username: req.body.username,
+        password: hashPassword(req.body.password),
+      });
+      newUser.save()
+      .catch((err) => {
+        console.log('Error in saving new user\n', err);
+        return res.status(500).json({
+          success: false,
+          message: 'Signup failed',
+        });
+      })
+      .then((user) => {
+        if (!user) {
+          return res.status(500).json({
+            success: false,
+            message: 'Signup did not save user',
+          });
+        }
+        return res.status(200).json({
+          success: true,
+          user,
+        });
+      });
+    });
 
-}
+    router.post('/login', (req, res, next) => {
+      req.body.username = req.body.email;
+      next();
+    }, passport.authenticate('local'), (req, res, next) => {
+      // TODO return some object for axios
+      res.json({
+        success: true,
+        user: req.user,
+      });
+    });
+
+    router.get('/logout', (req, res, next) => {
+      req.logout();
+      res.status(200).json({
+        success: true,
+      });
+    })
+    return router;
+  },
+  hashPassword,
+};
