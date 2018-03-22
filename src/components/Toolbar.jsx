8c76c5d5ab12e1aef074fcad914839ead12c1169
Chris Raveendra <craveend@umich.edu>
2018-03-21 21:53:53 -0700

    Updates to color picker

--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -21,11 +21,13 @@ import FormatAlignRight from 'material-ui/svg-icons/editor/format-align-right'
 
 import FormatSize from 'material-ui/svg-icons/editor/format-Size'
 import FormatColorText from 'material-ui/svg-icons/editor/format-color-text'
+import { SketchPicker, GithubPicker } from 'react-color'
 
 
 import FormatListBulleted from 'material-ui/svg-icons/editor/format-list-bulleted'
 import FormatListNumbered from 'material-ui/svg-icons/editor/format-list-Numbered'
 
+import Popover from 'material-ui/Popover'
 
 import createStyles from 'draft-js-custom-styles';
 import {connect} from 'react-redux';
@@ -39,7 +41,7 @@ const customStyleMap = {
 
 // Passing the customStyleMap is optional
 const { styles, customStyleFn, exporter } = createStyles([
-  'font-size', 'color', 'font-weight', 'font-style', 'text-decoration', 'text-align'
+  'font-size', 'color', 'font-weight', 'font-style', 'text-decoration', 'text-align', 'width', 'display', 'text-indent'
 ], 'CUSTOM_', customStyleMap);
 
 export default class Textbar extends React.Component {
@@ -62,10 +64,35 @@ export default class Textbar extends React.Component {
     this.handleEditorChange(newEditorState);
   };
 
+  toggleCenter = (e) => {
+    e.preventDefault();
+    let newEditorState = styles.textAlign.toggle(this.props.editorState, 'center');
+    newEditorState = styles.width.toggle(newEditorState, '100%');
+    newEditorState = styles.display.toggle(newEditorState, 'block');
+
+    // const newEditorState = RichUtils.toggleBlockType(this.props.editorState, 'center');
+    console.log(newEditorState.getCurrentInlineStyle().toJS());
+    this.handleEditorChange(newEditorState);
+  };
+
+  toggleBullet = (e) => {
+    e.preventDefault();
+    const newEditorState = styles.textIndent.toggle(this.props.editorState, '25');
+    console.log(newEditorState.getCurrentInlineStyle().toJS());
+    this.handleEditorChange(newEditorState);
+  };
+
   handleEditorChange = (editorState) => {
     // debugger;
     this.props.updateEditor(editorState);
   }
+
+  handleChangeComplete = (color) => {
+    console.log('old: ', this.props.editorState.getCurrentInlineStyle().toJS());
+    const newEditorState = styles.color.toggle(this.props.editorState, color.hex);
+    console.log('updated: ', newEditorState.getCurrentInlineStyle().toJS());
+    this.handleEditorChange(newEditorState);
+  };
 
   render() {
     let styles = this.props.editorState.getCurrentInlineStyle().toJS();
@@ -74,7 +101,7 @@ export default class Textbar extends React.Component {
         'position': 'sticky',
         'top': '0px'
       }}>
-        <Toolbar title="My Toolbar" style={{'display':'flex', 'alignItems': 'center'}}>
+        <Toolbar style={{'display':'flex', 'alignItems': 'center'}}>
           <FormatBold onMouseDown={this.toggleBold}
                       color={styles.includes("CUSTOM_FONT_WEIGHT_bold") ? 'black' : 'white'}
                     />
@@ -88,15 +115,56 @@ export default class Textbar extends React.Component {
 
           <FormatAlignLeft hoverColor={'black'} color={'white'}/>
 
-          <FormatAlignCenter hoverColor={'black'} color={'white'}/>
+          <FormatAlignCenter hoverColor={'black'} color={'white'} onMouseDown={this.toggleCenter}
+            color={styles.includes("CUSTOM_FONT_WEIGHT_bold") ? 'black' : 'white'}
+          />
 
           <FormatAlignRight hoverColor={'black'} color={'white'}/>
 
           <FormatSize hoverColor={'black'} color={'white'}/>
+          
+          {/* <div>
+          <RaisedButton
+            onClick={this.handleClick}
+            label="Click me"
+          />
+          <Popover
+            anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+            targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+            onRequestClose={this.handleRequestClose}
+          > */}
+          <IconMenu
+            iconButtonElement={<IconButton><FormatColorText /></IconButton>}
+            anchorOrigin={{ horizontal: 'left', vertical: 'top' }}
+            targetOrigin={{ horizontal: 'left', vertical: 'top' }}
+            >
 
-          <FormatColorText hoverColor={'black'} color={'black'}/>
+            <MenuItem
+              primaryText={< SketchPicker
+                onChangeComplete=
+                {this.handleChangeComplete}
+              />}
+            />
 
-          <FormatListBulleted hoverColor={'black'} color={'white'}/>
+            <MenuItem
+              primaryText="Case Tools"
+              menuItems={[
+                <MenuItem primaryText="UPPERCASE" />,
+                <MenuItem primaryText="lowercase" />,
+                <MenuItem primaryText="CamelCase" />,
+                <MenuItem primaryText="Propercase" />,
+              ]}
+            />
+            {/* <Divider /> */}
+            <MenuItem value="Del" primaryText="Customize" />
+
+          </IconMenu>
+          {/* </Popover>
+          </div> */}
+
+          {/* <FormatColorText hoverColor={'black'} color={'black'}/> */}
+
+          <FormatListBulleted hoverColor={'black'} color={'white'} onMouseDown={this.toggleBullet}/>
 
           <FormatListNumbered hoverColor={'black'} color={'white'}/>
 
