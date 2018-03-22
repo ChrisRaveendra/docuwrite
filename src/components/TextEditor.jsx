9d65b386b50ff99d9edc06f10847a091ad2873d1
Trevor <trevmanz94@gmail.com>
2018-03-21 21:48:10 -0700

    keyboard commands for bold, italics, and underline working in editor + visual toggle

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -7,6 +7,8 @@ import Paper from 'material-ui/Paper';
 import Textbar from './Toolbar';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 import createStyles from 'draft-js-custom-styles';
+import {getDefaultKeyBinding, KeyBindingUtil} from 'draft-js';
+const {hasCommandModifier} = KeyBindingUtil;
 
 const customStyleMap = {
   MARK: {
@@ -15,25 +17,30 @@ const customStyleMap = {
   }
 };
 
+//bind the cmd-b, cmd-i, and cmd-u keys to trigger style toggles
+const myKeyBindingFn = (e: SyntheticKeyboardEvent): string => {
+  if (e.keyCode === 66 /* `b` key */ && hasCommandModifier(e)) {
+    return 'bold';
+  }
+  if (e.keyCode === 73 /* `b` key */ && hasCommandModifier(e)) {
+    console.log('i hit')
+    return 'italic';
+  }
+  if (e.keyCode === 85 /* `b` key */ && hasCommandModifier(e)) {
+    console.log('u hit')
+    return 'underline';
+  }
+  return getDefaultKeyBinding(e);
+}
+
 // Passing the customStyleMap is optional
 const {styles, customStyleFn, exporter} = createStyles([
   'font-size', 'color', 'font-weight', 'font-style', 'text-decoration', 'text-align'
 ], 'CUSTOM_', customStyleMap);
 
 export default class TextEditor extends React.Component {
-  //onChange is used to update the state of the Draft.js editor for any toolbox changes (bold, italics, etc)
-  constructor(props) {
-    super(props);
-  }
   // Update editor state & selection state then pass these new states to the UPDATE action
   // This will result in the reducer signaling updates to the DOM
-
-  setAlign = () => {
-    // e.preventDefault();
-    const newEditorState = styles.textAlign.current(this.props.editorState);
-    console.log(newEditorState);
-    this.handleEditorChange(newEditorState);
-  }
 
   /*  Take in newly changed draftJS editor state & format correctly for use in redux Action
         Occurs for both:
@@ -44,9 +51,25 @@ export default class TextEditor extends React.Component {
   //  Tab exits the editor
   //  does show a selection state for bold/italic button click
   handleEditorChange = (editorState) => {
-    // debugger;
     this.props.updateEditor(editorState);
   }
+
+  handleKeyCommand = (command: string): DraftHandleValue => {
+    if (command === 'bold') {
+      const newEditorState = styles.fontWeight.toggle(this.props.editorState, 'bold');
+      this.handleEditorChange(newEditorState);
+    }
+    if (command === 'italic') {
+      const newEditorState = styles.fontStyle.toggle(this.props.editorState, 'italic');
+      this.handleEditorChange(newEditorState);
+    }
+    if (command === 'underline') {
+      const newEditorState = styles.textDecoration.toggle(this.props.editorState, 'underline');
+      this.handleEditorChange(newEditorState);
+    }
+    return 'not-handled';
+  }
+
 
   render() {
     return (<div id='content'>
@@ -62,7 +85,10 @@ export default class TextEditor extends React.Component {
                 onChange={this.handleEditorChange}
                 spellCheck={true} ref='editor'
                 customStyleFn={customStyleFn}
-                customStyleMap={customStyleMap} />
+                customStyleMap={customStyleMap}
+                handleKeyCommand={this.handleKeyCommand}
+                keyBindingFn={myKeyBindingFn}
+            />
       </Paper>
     </div>);
   }
